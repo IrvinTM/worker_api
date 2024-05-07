49f14f518324f7fb0ca6712d5a648274d745b893
IrvinTM <irvint698@gmail.com>
2024-05-06 20:26:41 -0600

    modified:   index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { G4F } from 'g4f'
 import cors from 'cors'
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 3000 ?? process.env.PORT;
 
 
 app.use(express.json());
