c563fea37ae12b18b9399cb7aa713b6fbb527f72
IrvinTM <irvint698@gmail.com>
2024-05-06 19:14:18 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { G4F } from 'g4f'
+import cors from 'cors'
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+
+app.use(express.json());
+app.use(cors())
+
+app.options('*', cors()) 
+app.post('/', async (req, res) => {
+    console.log(req.body)
+    const g4f = new G4F();
+        
+        const messages = [
+            { role: "user", content: req.body.texto}
+    ]
+    try{
+        const results = await g4f.chatCompletion(messages);
+        res.send(JSON.stringify(results));
+    }
+    catch (error){
+        console.error(error)
+    }
+    });
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:3000 port ${port}`);
+    });
