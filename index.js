3566badd818acf3989ccd641fc86a21b420f3edb
IrvinTM <irvint698@gmail.com>
2024-06-03 20:06:56 -0600

    added image generator

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000 ?? process.env.PORT;
 
 app.use(express.json());
 app.use(cors())
+app.use(express.text());
 
 app.options('*', cors()) 
 app.post('/', async (req, res) => {
@@ -24,10 +25,38 @@ app.post('/', async (req, res) => {
     }
     });
 
-    app.get('/', (req, res) => {
-        res.send('Hello World');
-    }
-    );
+    app.post("/image", (req, res) => {
+        const imagePrompt = req.body
+        const g4f = new G4F();
+        try{
+      (async () => {
+        const base64Image = await g4f.imageGeneration(imagePrompt, {
+          debug: true,
+          provider: g4f.providers.ProdiaStableDiffusion,
+          providerOptions: {
+            model: "epicrealism_naturalSinRC1VAE.safetensors [90a4c676]",
+            samplingSteps: 20,
+            cfgScale: 30,
+            height: 1024,
+            width: 1024,
+          },
+        });
+        const imageBuffer = Buffer.from(base64Image, "base64");
+
+        res.writeHead(200, {
+          "Content-Type": "image/jpeg",
+          "Content-Length": imageBuffer.length,
+        });
+
+        res.end(imageBuffer);
+
+      })();}
+      catch (error){
+        console.error(error)
+      }
+}
+
+);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:3000 port ${port}`);
